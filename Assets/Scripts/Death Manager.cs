61bb8157f433959f36d1271a86c0e20719952ade
EtheriumBot <ethanrwu@gmail.com>
2026-04-18 19:05:20 -0700

    death screen + raycasting works

--- Assets/Scripts/Death Manager.cs
+++ Assets/Scripts/Death Manager.cs
@@ -3,15 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class DeathManager : MonoBehaviour
 {
-    private WaterManager waterManager;
-    private PlayerOverheatLogic playerOverheatLogic;
+    private WaterManager wm;
+    private PlayerOverheatLogic pol;
     private bool isDead = false; // Prevents the scene from loading multiple times
 
     void Start()
     {
         // Keep the references in Start so we only find them once
-        waterManager = WaterManager.reference;
-        playerOverheatLogic = PlayerOverheatLogic.reference;
+        wm = WaterManager.reference;
+        pol = PlayerOverheatLogic.reference;
     }
 
     void Update()
@@ -26,7 +26,7 @@ public class DeathManager : MonoBehaviour
     void CheckDeathConditions()
     {
         // Check if water is empty OR heat is full
-        if (waterManager.waterLevel <= 0.01f || playerOverheatLogic.overheat >= 99.9f)
+        if (wm.waterLevel <= 0.01f || pol.overheat >= 99.9f)
         {
             isDead = true; // Set to true so this block doesn't trigger again
 
